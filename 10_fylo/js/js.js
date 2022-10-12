fbc0aa2ec355240085f6c3bb8081f94b474943df
tsotneforester <tsotne.meladze.usa@gmail.com>
2022-10-12 13:48:39 +0400

    10 - js added

--- 10_fylo/js/js.js
+++ 10_fylo/js/js.js
@@ -1,1 +1,17 @@
 // var redBox = document.getElementById('...')
+"use strict";
+
+const submit = document.querySelectorAll("input[type='submit']");
+const input = document.querySelectorAll("input[type='email']");
+const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+for (let i = 0; i < 2; i++) {
+  submit[i].addEventListener("click", function () {
+    let text = input[i].value;
+    if (!reg.test(text)) {
+      input[i].style.outline = "#EF4877 solid 0.7px";
+    } else {
+      input[i].removeAttribute("style");
+    }
+  });
+}
