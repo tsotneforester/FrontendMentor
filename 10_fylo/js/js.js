6c495a18b47cff7b0d8b6e20b252081f77eca5b5
tsotneforester <tsotne.meladze.usa@gmail.com>
2022-10-20 21:11:35 +0400

    fixes #8

--- 10_fylo/js/js.js
+++ 10_fylo/js/js.js
@@ -3,6 +3,7 @@
 
 const submit = document.querySelectorAll("input[type='submit']");
 const input = document.querySelectorAll("input[type='email']");
+const h6 = document.querySelectorAll("h6");
 const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 for (let i = 0; i < 2; i++) {
@@ -10,8 +11,10 @@ for (let i = 0; i < 2; i++) {
     let text = input[i].value;
     if (!reg.test(text)) {
       input[i].style.border = "#EF4877 solid 1px";
+      h6[i].removeAttribute("style");
     } else {
       input[i].removeAttribute("style");
+      h6[i].style.display = "none";
     }
   });
 }
