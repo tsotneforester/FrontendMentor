1dfc0cbb73e1ef83c6d6e6aa528d9773588aeb66
tsotneforester <tsotne.meladze.usa@gmail.com>
2022-10-26 06:35:06 +0400

    11 js reconstructed

--- 11_tip_calculator/js/js.js
+++ 11_tip_calculator/js/js.js
@@ -1,134 +1,134 @@
 "use strict";
 let tipButton = document.querySelectorAll(".tipbutton");
 let resetButton = document.querySelector("#resetbutton");
+let person = document.querySelector("#person_input");
+let tip = document.querySelector("#tipAmount");
+let total = document.querySelector("#tipAmount");
+let bill = document.querySelector("#bill_input");
+let error = document.querySelectorAll(".errorTitle");
 
-let billAmount = 0;
-let tipPercent = 0;
-let peopleAmount = 0;
-let tipAmount = 0;
-let totalAmount = 0;
-
-function activateReset() {
-  resetButton.style.backgroundColor = "#26c2ae";
-}
+const Cal = {
+  numVars: {
+    billAmount: 0,
+    tipPercent: 0,
+    peopleAmount: 0,
+    tipAmount: 0,
+    totalAmount: 0,
+  },
+  removeZero(e) {
+    error[e].innerHTML = "";
+  },
+  activateReset() {
+    resetButton.style.backgroundColor = "#26c2ae";
+  },
+  removeClick() {
+    for (let i = 0; i < tipButton.length; i++) {
+      tipButton[i].classList.remove("clicked");
+    }
+  },
+  checkZero() {
+    if (Cal.numVars.billAmount == 0) {
+      error[0].innerHTML = "Can’t be zero";
+      bill.classList.add("errorborder");
+    }
+    if (Cal.numVars.tipPercent == 0) {
+      error[1].innerHTML = "Can’t be zero";
+    }
+    if (Cal.numVars.peopleAmount == 0) {
+      error[2].innerHTML = "Can’t be zero";
+      person.classList.add("errorborder");
+    }
+  },
+  billClick() {
+    if (this.numVars.billAmount == 0) {
+      this.checkZero();
+    }
+    this.activateReset();
+  },
+};
 
 function disableReset() {
   resetButton.removeAttribute("style");
 }
 
-function removeClick() {
-  for (let i = 0; i < tipButton.length; i++) {
-    tipButton[i].classList.remove("clicked");
-  }
-}
-
 function calculateTip() {
-  if (billAmount != 0 && tipPercent != 0 && peopleAmount != 0) {
-    tipAmount = (billAmount * tipPercent) / 100;
-    totalAmount = tipAmount / peopleAmount;
-    document.querySelector("#tipAmount").innerHTML = tipAmount.toFixed(2);
-    document.querySelector("#totalAmount").innerHTML = totalAmount.toFixed(2);
+  if (Cal.numVars.billAmount != 0 && Cal.numVars.tipPercent != 0 && Cal.numVars.peopleAmount != 0) {
+    Cal.numVars.tipAmount = (Cal.numVars.billAmount * Cal.numVars.tipPercent) / 100;
+    Cal.numVars.totalAmount = Cal.numVars.tipAmount / Cal.numVars.peopleAmount;
+    tip.innerHTML = Cal.numVars.tipAmount.toFixed(2);
+    total.innerHTML = Cal.numVars.totalAmount.toFixed(2);
   }
-  if (billAmount == 0 || tipPercent == 0 || peopleAmount == 0) {
-    // tipAmount = 0;
-    // totalAmount = 0;
-    document.querySelector("#tipAmount").innerHTML = "0.00";
-    document.querySelector("#totalAmount").innerHTML = "0.00";
+  if (Cal.numVars.billAmount == 0 || Cal.numVars.tipPercent == 0 || Cal.numVars.peopleAmount == 0) {
+    tip.innerHTML = "0.00";
+    total.innerHTML = "0.00";
   }
 }
 
 function resetAmounts() {
-  billAmount = 0;
-  tipPercent = 0;
-  peopleAmount = 0;
-  tipAmount = 0;
-  totalAmount = 0;
-  removeZero(0);
-  removeZero(1);
-  removeZero(2);
-  document.querySelector("#bill_input").value = 0;
-  document.querySelector("#person_input").value = 0;
-  document.querySelector("#tipAmount").innerHTML = "0.00";
-  document.querySelector("#totalAmount").innerHTML = "0.00";
-  removeClick();
+  Cal.numVars.billAmount = 0;
+  Cal.numVars.tipPercent = Cal.numVars.peopleAmount = Cal.numVars.tipAmount = Cal.numVars.totalAmount = 0;
+
+  Cal.removeZero(0);
+  Cal.removeZero(1);
+  Cal.removeZero(2);
+  bill.value = "";
+  person.value = "";
+  tip.innerHTML = "0.00";
+  total.innerHTML = "0.00";
+  Cal.removeClick();
   disableReset();
-  document.querySelector("#bill_input").classList.remove("errorborder");
-  document.querySelector("#person_input").classList.remove("errorborder");
-}
-
-function checkZero() {
-  if (billAmount == 0) {
-    document.querySelectorAll(".errorTitle")[0].innerHTML = "Can’t be zero";
-    document.querySelector("#bill_input").classList.add("errorborder");
-  }
-  if (tipPercent == 0) {
-    document.querySelectorAll(".errorTitle")[1].innerHTML = "Can’t be zero";
-  }
-  if (peopleAmount == 0) {
-    document.querySelectorAll(".errorTitle")[2].innerHTML = "Can’t be zero";
-    document.querySelector("#person_input").classList.add("errorborder");
-  }
-}
-
-function removeZero(e) {
-  document.querySelectorAll(".errorTitle")[e].innerHTML = "";
+  bill.classList.remove("errorborder");
+  person.classList.remove("errorborder");
 }
 
 for (let i = 0; i < tipButton.length; i++) {
   tipButton[i].addEventListener("click", (e) => {
-    removeClick();
+    Cal.removeClick();
     e.target.classList.add("clicked");
-    tipPercent = Number(tipButton[i].getAttribute("value"));
-    checkZero();
-    activateReset();
+    Cal.numVars.tipPercent = Number(tipButton[i].getAttribute("value"));
+    Cal.checkZero();
+    Cal.activateReset();
     calculateTip();
-    activateReset();
-    if (tipPercent != 0) {
-      removeZero(1);
+    Cal.activateReset();
+    if (Cal.numVars.tipPercent != 0) {
+      Cal.removeZero(1);
     }
   });
 }
 
-document.querySelector("#bill_input").addEventListener("click", () => {
-  if (billAmount == 0) {
-    checkZero();
-  }
-  activateReset();
-});
-
-document.querySelector("#bill_input").addEventListener("input", () => {
-  billAmount = Number(document.querySelector("#bill_input").value);
+bill.addEventListener("input", () => {
+  Cal.numVars.billAmount = Number(bill.value);
   calculateTip();
-  if (billAmount != 0) {
-    removeZero(0);
-    document.querySelector("#bill_input").classList.remove("errorborder");
+  if (Cal.numVars.billAmount != 0) {
+    Cal.removeZero(0);
+    bill.classList.remove("errorborder");
   }
 
-  if (billAmount == 0) {
-    checkZero();
-    document.querySelector("#bill_input").classList.add("errorborder");
+  if (Cal.numVars.billAmount == 0) {
+    Cal.checkZero();
+    bill.classList.add("errorborder");
   }
 });
 
-document.querySelector("#person_input").addEventListener("click", () => {
-  if (peopleAmount == 0) {
-    checkZero();
+person.addEventListener("click", () => {
+  if (Cal.numVars.peopleAmount == 0) {
+    Cal.checkZero();
   }
-  activateReset();
+  Cal.activateReset();
 });
 
-document.querySelector("#person_input").addEventListener("input", () => {
-  peopleAmount = Number(document.querySelector("#person_input").value);
+person.addEventListener("input", () => {
+  Cal.numVars.peopleAmount = Number(person.value);
 
   calculateTip();
-  if (peopleAmount != 0) {
-    removeZero(2);
-    document.querySelector("#person_input").classList.remove("errorborder");
+  if (Cal.numVars.peopleAmount != 0) {
+    Cal.removeZero(2);
+    person.classList.remove("errorborder");
   }
 
-  if (peopleAmount == 0) {
-    checkZero();
-    document.querySelector("#person_input").classList.add("errorborder");
+  if (Cal.numVars.peopleAmount == 0) {
+    Cal.checkZero();
+    person.classList.add("errorborder");
   }
 });
 
