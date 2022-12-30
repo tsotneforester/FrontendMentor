e6d764732250a1971267d439c6434801fb0e7cf1
tsotneforester <tsotne.meladze.usa@gmail.com>
2022-12-30 22:17:28 +0400

    automated

--- 06_art_gallery/leaflet/waypoint.js
+++
@@ -1,9 +0,0 @@
-//---------------------------Bitcamp----------
-
-var marker1 = L.marker([41.69575, 44.79658], { icon: icon1 }).bindPopup("<a href='https://www.bitcamp.ge/' target='_blank'>Bitcamp</a>");
-
-//-------------------------------------
-var monishvnebi_layer = L.layerGroup([/* marker1, */ marker1]);
-
-// side 0.16/0.08 top 0.105 (y,x)
-// 42.46602 , 44.88739
