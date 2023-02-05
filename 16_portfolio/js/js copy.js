90f9e287f2aa31b87129bf97b3920f5cfb75f279
tsotneforester <tsotne.meladze.usa@gmail.com>
2023-02-05 14:27:10 +0400

    16_portfolio

--- 16_portfolio/js/js copy.js
+++ 16_portfolio/js/js copy.js
@@ -8,32 +8,29 @@
 let img = document.querySelector(".slide img");
 let imgWidth = parseInt(getComputedStyle(img).width);
 let slide = document.querySelector(".slide");
-let slideGap = parseInt(getComputedStyle(slide).gap);
-let step = imgWidth + slideGap;
 let next = document.getElementById("next");
 let previous = document.getElementById("previous");
 let currentSlide = 0;
+console.log(currentSlide);
 
 next.addEventListener("click", function () {
   currentSlide++;
+  console.log(currentSlide);
   if (currentSlide == 3) {
     currentSlide = 2;
-    slide.style.transform = `translateX(-${currentSlide * step}px)`;
+    slide.style.transform = `translateX(-${currentSlide * imgWidth}px)`;
   } else if (currentSlide > -1) {
-    slide.style.transform = `translateX(-${currentSlide * step}px)`;
+    slide.style.transform = `translateX(-${currentSlide * imgWidth}px)`;
   } else {
-    slide.style.transform = `translateX(${Math.abs(currentSlide) * step}px)`;
+    slide.style.transform = `translateX(${Math.abs(currentSlide) * imgWidth}px)`;
   }
 });
 
 previous.addEventListener("click", function () {
   currentSlide--;
-  if (currentSlide == -3) {
-    currentSlide = -2;
-    slide.style.transform = `translateX(-${currentSlide * step}px)`;
-  } else if (currentSlide > -1) {
-    slide.style.transform = `translateX(-${currentSlide * step}px)`;
+  if (currentSlide > -1) {
+    slide.style.transform = `translateX(-${currentSlide * imgWidth}px)`;
   } else {
-    slide.style.transform = `translateX(${Math.abs(currentSlide) * step}px)`;
+    slide.style.transform = `translateX(${Math.abs(currentSlide) * imgWidth}px)`;
   }
 });
