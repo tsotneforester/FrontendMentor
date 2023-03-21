6287b7559cbc6c4bcdf205549efee01dd6d9710f
tsotneforester <tsotne.meladze.usa@gmail.com>
2023-03-21 11:30:45 +0400

    automated

--- 19_interactive_card/js/js.js
+++ 19_interactive_card/js/js.js
@@ -9,16 +9,22 @@ let cvcInput = document.getElementById("cvc");
 let cvcEngraved = document.getElementById("engraved-cvc");
 let numberInput = document.getElementById("number");
 let numberEngraved = document.getElementById("engraved-number");
+let nameInput = document.getElementById("name");
+let nameEngraved = document.getElementById("engraved-name");
+let monthInput = document.getElementById("month");
+let monthEngraved = document.getElementById("engraved-month");
+let yearInput = document.getElementById("year");
+let yearEngraved = document.getElementById("engraved-year");
 cvcInput.addEventListener("input", function () {
-  console.log("first");
   cvcEngraved.innerHTML = cvcInput.value;
+  cvcInput.value = cvcInput.value.replace(/[^0-9]/g, "");
 });
 
 numberInput.addEventListener("input", function (e) {
   let raw = e.target.value.replaceAll(" ", "");
-
+  console.log(raw);
   if (raw) {
-    let rawReg = raw.match(/\d{4}/g);
+    let rawReg = raw.match(/\w{4}/g);
     let result = rawReg ? zorg(raw) : e.target.value;
 
     numberEngraved.innerHTML = result;
@@ -28,25 +34,73 @@ numberInput.addEventListener("input", function (e) {
   }
 });
 
-let text = "12345";
-let result = text.match(/\d{4}/g) || [];
+nameInput.addEventListener("input", function () {
+  nameEngraved.innerHTML = nameInput.value;
+});
 
-console.log(result.join(" "));
+monthInput.addEventListener("input", function () {
+  monthEngraved.innerHTML = monthInput.value;
+  monthInput.value = monthInput.value.replace(/[^0-9]/g, "");
+  if (monthInput.value > 12) {
+    monthInput.value = 12;
+    monthEngraved.innerHTML = 12;
+  }
+});
+
+yearInput.addEventListener("input", function () {
+  yearEngraved.innerHTML = yearInput.value;
+  yearInput.value = yearInput.value.replace(/[^0-9]/g, "");
+});
 
 function zorg(string) {
   let zzz = [...string];
+  console.log(string);
   let n = Math.floor(zzz.length / 4);
   console.log(n);
   for (let i = 0; i < n; i++) {
     zzz.splice(4 + 4 * i + i, 0, " ");
-    console.log("object");
   }
   return zzz.join("");
 }
 
-console.log(zorg("1234252242562345"));
+let submit = document.getElementById("submit");
+submit.addEventListener("click", function () {
+  let allInput = document.querySelectorAll("input");
+  let allError = document.querySelectorAll(".error");
 
-// let sss = "1234-56675";
-// let zzz = [...sss];
-// zzz.splice(9, 0, "-");
-// console.log(zzz.join(""));
+  for (let i = 0; i < allInput.length; i++) {
+    if (allInput[i].value == "") {
+      allError[i].innerHTML = "Can`t be blank";
+      allError[i].style.height = "20px";
+    } else {
+      allError[i].style.height = "0";
+    }
+  }
+
+  if (numberInput.value.replaceAll(" ", "").length > 1 && numberInput.value.replaceAll(" ", "").length < 16) {
+    allError[1].innerHTML = "Number is too short";
+    allError[1].style.height = "20px";
+  }
+
+  if (numberInput.value.replaceAll(" ", "").length == 16) {
+    let pattern = /\D+/g;
+
+    if (pattern.test(numberInput.value.replaceAll(" ", ""))) {
+      allError[1].innerHTML = "Wrong format, numbers only";
+      allError[1].style.height = "20px";
+    }
+
+    if (!pattern.test(numberInput.value.replaceAll(" ", ""))) {
+      allError[i].style.height = "0";
+    }
+  }
+
+  if (cvcInput.value.length < 3) {
+    allError[4].innerHTML = "Number is too short";
+    allError[4].style.height = "20px";
+  }
+
+  if (cvcInput.value.length > 2) {
+    allError[4].style.height = "0";
+  }
+});
